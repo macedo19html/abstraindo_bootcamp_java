11d255c9380f3caa0d00571333de1a6e1fa92845
joao macedo <73135265+macedo19html@users.noreply.github.com>
2024-06-05 11:51:08 -0300

    Add files via upload
    muito bom!!!

---
+++ desafio_abstraindo_um_bootcamp_java/Curso.java
@@ -0,0 +1,31 @@
+package desafio_abstraindo_um_bootcamp_java;
+
+public class Curso extends Conteudo {
+	private int cargaHoraria;
+	
+	 @Override
+	    public double calcularXp() {
+	        return XP_PADRAO * cargaHoraria;
+	    }
+
+	    public Curso() {
+	    }
+
+
+	    public int getCargaHoraria() {
+	        return cargaHoraria;
+	    }
+
+	    public void setCargaHoraria(int cargaHoraria) {
+	        this.cargaHoraria = cargaHoraria;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Curso{" +
+	                "titulo='" + getTitulo() + '\'' +
+	                ", descricao='" + getDescricao() + '\'' +
+	                ", cargaHoraria=" + cargaHoraria +
+	                '}';
+	    }
+}
